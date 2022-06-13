8bc2aa286e7c862a0d5154e0fe914363b5358ab4
CaoHoangKiet222 <caohoangkiet1720@gmail.com>
2022-06-13 15:47:52 +0700

    Change delete method for conversation model

--- server/controllers/messages.js
+++ server/controllers/messages.js
@@ -1,19 +1,13 @@
+const { default: mongoose } = require("mongoose");
 const Conversation = require("../models/conversation");
 
 exports.deleteMessage = async (req, res, _next) => {
   try {
-    const conversation = await Conversation.findById(
-      req.body.conversationId
-    ).select("messages");
-
-    conversation.messages.splice(
-      conversation.messages.findIndex(
-        ({ _id }) => _id.toString() === req.body.message._id.toString()
-      ),
-      1
-    );
-
-    await conversation.save();
+    await Conversation.findByIdAndUpdate(req.body.conversationId, {
+      $pull: {
+        messages: { _id: req.body.message._id },
+      },
+    });
   } catch (error) {
     console.log(error);
   }
