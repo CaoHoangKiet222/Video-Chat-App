5f86dd1e346ee4a6cc622adf3bc365d3c1ed8a5b
CaoHoangKiet222 <caohoangkiet1720@gmail.com>
2022-07-21 16:24:27 +0700

    new branch client for videochatapp

--- server/routes/meetings.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const meetingsController = require("../controllers/meetings");
-
-router.get("/get-calls", meetingsController.getCalls);
-
-module.exports = router;
