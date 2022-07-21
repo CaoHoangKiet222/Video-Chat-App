a811e507df70012d9cd96da5e0d5d18ecf69cf54
Caohoangkiet2k2 <86540039+CaoHoangKiet222@users.noreply.github.com>
2022-07-21 22:27:51 +0700

    add accept res.header in server

--- app.js
+++ app.js
@@ -44,6 +44,14 @@ app.use(
   })
 );
 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    next();
+});
+
 app.get("/", (_req, res) => {
   res.send("Server is running");
 });
